4ae97c071faa49ea2b65e6322e059db03b66f7b1
unknown <mradamknapp@gmail.com>
2018-04-10 13:19:37 -0500

    initial

---
+++ SQL HOMEWORK Scripts.sql
@@ -0,0 +1,7 @@
+-- **** ADAM KNAPP'S SQL HOMEWORK SCRIPTS from MySQL !! ****
+
+use sakila;
+
+-- 1a. Display the first and last names of all actors from the table `actor`. 
+
+select first_name, last_name
